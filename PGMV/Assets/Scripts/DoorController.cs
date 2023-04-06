88221fa76e1f50b666f3f416c3e74a22bf993b52
ampmc-iscte <ampmc@iscte-iul.pt>
2023-04-06 14:21:38 +0100

    v1

---
+++ PGMV/Assets/Scripts/DoorController.cs
@@ -0,0 +1,18 @@
+using System.Numerics;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    public GameObject pivot;
+
+    private bool isOpen = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !isOpen)
+        {
+            isOpen = true;
+            pivot.transform.rotation = UnityEngine.Quaternion.Euler(-90f, 0f, 0f);
+        }
+    }
+}
