48b2ba16b50361af85567f77da3137dba2b9897f
dmcrs2-iscte <dmcrs2@iscte-iul.pt>
2023-05-09 05:31:44 +0100

    Guia, Velocímetro e Botões de garagem no HUD, Faróis e Turbo
    Guia está minimamente funcional mas é preciso alguns ajustes. Velocímetro está no HUD. Botões para escolha de garagens para aparecer o objeto guia. O carro já tem faróis (para ligar é o L). Carro tem turbo mas precisa de ser ajustado um bug (para ligar é o shift esquerdo).

---
+++ PGMV/Assets/Scripts/SetNavAgentToGarage.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SetNavAgentToGarage : MonoBehaviour
+{
+
+    public GameObject garage;
+
+    public GameObject NavAgent;
+
+    public GameObject car;
+
+    private GameObject guide;
+
+    private NavMeshAgent agent;
+
+
+    void FixedUpdate()
+    {
+        if (agent != null)
+        {
+            if (!agent.pathPending)
+            {
+                if (agent.remainingDistance <= agent.stoppingDistance)
+                {
+                    if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+                    {
+                        Destroy(guide);
+                    }
+                }
+            }
+        }
+
+
+    }
+
+    public void SpawnAgent()
+    {
+        //isActive = NavAgent.activeSelf;
+        //NavAgent.SetActive(!isActive);
+        if (!GameObject.Find("Guide(Clone)"))
+        {
+            guide = Instantiate(NavAgent, car.transform.position+(car.transform.forward*6), car.transform.rotation);
+        }
+        //NavAgent.transform.position = new Vector3(car.transform.position.x, car.transform.position.y + 1, car.transform.position.z + 8);
+        if (GameObject.Find("Guide(Clone)"))
+        {
+            agent = guide.GetComponent<NavMeshAgent>();
+            agent.destination = garage.transform.Find("Door Pivot").Find("Door").position;
+        }
+    }
+}
